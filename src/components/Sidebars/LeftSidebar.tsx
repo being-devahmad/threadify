f1ceff3796695ca40f51cca238bac81012d965ef
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2025-02-03 00:42:25 +0500

    basic layout setup

---
+++ src/components/Sidebars/LeftSidebar.tsx
@@ -0,0 +1,63 @@
+import { menuList } from "@/constants";
+import Link from "next/link";
+
+
+const LeftSidebar = () => {
+    return (
+        <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+            {/* LOGO MENU BUTTON */}
+            <div className="flex flex-col gap-4 text-lg items-center xxl:items-start">
+                {/* LOGO */}
+                <Link href="/" className="p-2 rounded-full hover:bg-[#181818] ">
+                    {/* <Image path="icons/logo.svg" alt="logo" w={24} h={24} /> */}
+                </Link>
+                {/* MENU LIST */}
+                <div className="flex flex-col gap-4">
+                    {menuList.map((item) => (
+                        <Link
+                            href={item.link}
+                            className="p-2 rounded-full hover:bg-[#181818] flex items-center gap-4"
+                            key={item.id}
+                        >
+                            {/* <Image
+                                path={`icons/${item.icon}`}
+                                alt={item.name}
+                                w={24}
+                                h={24}
+                            /> */}
+                            <span className="hidden xxl:inline">{item.name}</span>
+                        </Link>
+                    ))}
+                </div>
+                {/* BUTTON */}
+                <Link
+                    href="/compose/post"
+                    className="bg-white text-black rounded-full w-12 h-12 flex items-center justify-center xxl:hidden"
+                >
+                    {/* <Image path="icons/post.svg" alt="new post" w={24} h={24} /> */}
+                </Link>
+                <Link
+                    href="/compose/post"
+                    className="hidden xxl:block bg-white text-black rounded-full font-bold py-2 px-20"
+                >
+                    Post
+                </Link>
+            </div>
+            {/* USER */}
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                    <div className="w-10 h-10 relative rounded-full overflow-hidden">
+                        {/* <Image path="/general/avatar.png" alt="lama dev" w={100} h={100} tr={true} /> */}
+                    </div>
+                    <div className="hidden xxl:flex flex-col">
+                        <span className="font-bold">Lama Dev</span>
+                        <span className="text-sm text-textGray">@lamaWebDev</span>
+                    </div>
+                </div>
+                <div className="hidden xxl:block cursor-pointer font-bold">...</div>
+            </div>
+        </div>
+    );
+};
+
+export default LeftSidebar;
